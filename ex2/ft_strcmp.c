2ac80f14fc846342322446e0692c5d47a77e7c43
Motch <abomeezo2@gmail.com>
2026-08-17 00:54:30 +0300

    ex2

---
+++ ex2/ft_strcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/16 12:00:36 by mohammah          #+#    #+#             */
+/*   Updated: 2026/08/17 00:24:46 by mohammah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	index;
+	int	result;
+
+	index = 0;
+	while (s1[index])
+	{
+		if (s1[index] == s2[index])
+			index++;
+		else 
+		{
+			if (s1[index] > s2[index])
+				return (1);
+			else
+				return (-1);
+			index++;
+		}
+	
+	}
+	return (0);
+}
