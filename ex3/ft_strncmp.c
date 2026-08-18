22a09ed9728a6d1b10284b1716666d5bd67af26a
Dead Signal <abomeezo2@gmail.com>
2026-08-18 11:03:04 +0300

    fix: correct ft_strncmp comparison

--- ex3/ft_strncmp.c
+++ ex3/ft_strncmp.c
@@ -6,28 +6,20 @@
 /*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 00:25:54 by mohammah          #+#    #+#             */
-/*   Updated: 2026/08/17 00:56:26 by mohammah         ###   ########.fr       */
+/*   Updated: 2026/08/18 11:02:00 by mohammah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	index;
+	unsigned int	index;
 
-	while (index < n)
-	{
-		if (s1[index] && s2[index])
-		{
-			if (s1[index] == s2[index])
-				continue ;
-			else if (s1[index] > s2[index])
-				return (1);
-			else
-				return (-1);
-		}
-		else
-			break ;
+	index = 0;
+	while (index < n && s1[index] && s1[index] == s2[index])
 		index++;
-	}
-	return (0);
+	if (index == n || s1[index] == s2[index])
+		return (0);
+	if (s1[index] > s2[index])
+		return (1);
+	return (-1);
 }
