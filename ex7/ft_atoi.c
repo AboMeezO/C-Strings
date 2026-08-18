3e9cf4797f5340222040fe48e781fcc36ceae59b
Dead Signal <abomeezo2@gmail.com>
2026-08-18 11:03:18 +0300

    feat: implement ft_atoi

--- ex7/ft_atoi.c
+++ ex7/ft_atoi.c
@@ -6,15 +6,32 @@
 /*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 01:42:58 by mohammah          #+#    #+#             */
-/*   Updated: 2026/08/17 14:43:54 by mohammah         ###   ########.fr       */
+/*   Updated: 2026/08/18 11:02:00 by mohammah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 int	ft_atoi(char *str)
 {
 	int	index;
+	int	sign;
 	int	number;
 
-	while ()
+	index = 0;
+	sign = 1;
+	number = 0;
+	while (str[index] == ' ' || str[index] == '\t' || str[index] == '\n'
+		|| str[index] == '\v' || str[index] == '\f' || str[index] == '\r')
+		index++;
+	while (str[index] == '+' || str[index] == '-')
 	{
+		if (str[index] == '-')
+			sign *= -1;
+		index++;
 	}
+	while (str[index] >= '0' && str[index] <= '9')
+	{
+		number = number * 10 + str[index] - '0';
+		index++;
+	}
+	return (number * sign);
 }
