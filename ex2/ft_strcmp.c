0f6903f6563b059ba152151a46dc0e9d16c670b0
Dead Signal <abomeezo2@gmail.com>
2026-08-18 11:02:59 +0300

    fix: correct ft_strcmp comparison

--- ex2/ft_strcmp.c
+++ ex2/ft_strcmp.c
@@ -6,28 +6,20 @@
 /*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 12:00:36 by mohammah          #+#    #+#             */
-/*   Updated: 2026/08/17 14:53:17 by mohammah         ###   ########.fr       */
+/*   Updated: 2026/08/18 11:02:00 by mohammah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	index;
-	int	result;
 
 	index = 0;
-	while (s1[index])
-	{
-		if (s1[index] == s2[index])
-			index++;
-		else
-		{
-			if (s1[index] > s2[index])
-				return (1);
-			else
-				return (-1);
-			index++;
-		}
-	}
-	return (0);
+	while (s1[index] && s1[index] == s2[index])
+		index++;
+	if (s1[index] == s2[index])
+		return (0);
+	if (s1[index] > s2[index])
+		return (1);
+	return (-1);
 }
